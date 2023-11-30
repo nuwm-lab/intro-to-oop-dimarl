6a273766117198fcf04827cd3fce195eaad9cf34
dimarl <115631397+dimarl@users.noreply.github.com>
2023-11-30 20:54:30 +0300

    Update lab4.cs

--- lab_4/lab4.cs
+++ lab_4/lab4.cs
@@ -1,212 +1,116 @@
 using System;
 
-namespace Inheritance
+namespace Geometry
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double[,] verticesTrian = new double[Triangle.CountOfVerticesTrian, 2];
-            double[,] verticesQuad = new double[Quadrangle.CountOfVerticesQuad, 2];
+            double[,] verticesTrian = new double[Triangle.VertexCount, 2];
+            double[,] verticesQuad = new double[Quadrangle.VertexCount, 2];
             string[] nameVerticesTrian = { "A", "B", "C" };
             string[] nameVerticesQuad = { "A", "B", "C", "D" };
             Triangle trian = new Triangle();
             Quadrangle quad = new Quadrangle();
 
-            for (int i = 0; i < Triangle.CountOfVerticesTrian; i++)
-            {
-                Console.WriteLine($"Введіть координати для вершини {nameVerticesTrian[i]}: ");
-                verticesTrian[i, 0] = Convert.ToDouble(Console.ReadLine());
-                verticesTrian[i, 1] = Convert.ToDouble(Console.ReadLine());
-            }
-
-            trian.SetVertices(verticesTrian);
+            InputVertices(trian, nameVerticesTrian, verticesTrian);
             trian.ShowVertices();
             trian.CalculateSides();
-            Console.WriteLine($"Площа трикутника: {trian.CalculateArea()}");
+            Console.WriteLine($"Area of the triangle: {trian.CalculateArea()}");
 
-            for (int i = 0; i < Quadrangle.CountOfVerticesQuad; i++)
-            {
-                Console.WriteLine($"Введіть координати для вершини {nameVerticesQuad[i]}: ");
-                verticesQuad[i, 0] = Convert.ToDouble(Console.ReadLine());
-                verticesQuad[i, 1] = Convert.ToDouble(Console.ReadLine());
-            }
-
-            quad.SetVertices(verticesQuad);
+            InputVertices(quad, nameVerticesQuad, verticesQuad);
             quad.ShowVertices();
             quad.CalculateSides();
-            Console.WriteLine($"Площа чотирикутника: {quad.CalculateArea()}");
+            Console.WriteLine($"Area of the quadrangle: {quad.CalculateArea()}");
+        }
+
+        static void InputVertices(Shape shape, string[] names, double[,] vertices)
+        {
+            for (int i = 0; i < shape.VertexCount; i++)
+            {
+                Console.WriteLine($"Enter coordinates for vertex {names[i]}: ");
+                vertices[i, 0] = Convert.ToDouble(Console.ReadLine());
+                vertices[i, 1] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            shape.SetVertices(vertices);
         }
     }
-}
 
-using System;
-
-namespace Inheritance
-{
-    class Triangle
+    class Shape
     {
-        private static int CountOfVert = 3;
-        public static int CountOfVerticesTrian
-        {
-            get { return CountOfVert; }
-        }
+        protected double[,] vertices;
+        public int VertexCount { get; protected set; }
 
-        private double[,] vertices;
-        private double[] lengthOfSides;
-        private string[] nameOfSides = { "AB", "BC", "AC" };
-
-        public void SetVertices(double[,] vertices)
+        public virtual void SetVertices(double[,] vertices)
         {
-            this.vertices = new double[CountOfVert, 2];
-            for (int i = 0; i < CountOfVert; i++)
-            {
-                this.vertices[i, 0] = vertices[i, 0];
-                this.vertices[i, 1] = vertices[i, 1];
-            }
+            this.vertices = vertices;
         }
 
         public void ShowVertices()
         {
             if (vertices == null)
             {
-                Console.WriteLine("Координати не встановлені!");
+                Console.WriteLine("Coordinates not set!");
             }
             else
             {
                 Console.WriteLine("-----------------------------");
-                for (int i = 0; i < CountOfVert; i++)
+                for (int i = 0; i < VertexCount; i++)
                 {
-                    Console.WriteLine($"Вершина {i + 1}: x = {vertices[i, 0]}, y = {vertices[i, 1]}");
+                    Console.WriteLine($"Vertex {i + 1}: x = {vertices[i, 0]}, y = {vertices[i, 1]}");
                 }
                 Console.WriteLine("-----------------------------");
             }
         }
 
-        public void CalculateSides()
+        public virtual void CalculateSides()
         {
-            Console.WriteLine("-----------------------------");
-            if (vertices == null)
-            {
-                Console.WriteLine("Координати не встановлені!");
-                Console.WriteLine("-----------------------------");
-            }
-            else
-            {
-                lengthOfSides = new double[CountOfVerticesTrian];
-                this.lengthOfSides[0] = Math.Sqrt(Math.Pow((vertices[1, 0] - vertices[0, 0]), 2) + Math.Pow((vertices[1, 1] - vertices[0, 1]), 2));
-                this.lengthOfSides[1] = Math.Sqrt(Math.Pow((vertices[2, 0] - vertices[1, 0]), 2) + Math.Pow((vertices[2, 1] - vertices[1, 1]), 2));
-                this.lengthOfSides[2] = Math.Sqrt(Math.Pow((vertices[2, 0] - vertices[0, 0]), 2) + Math.Pow((vertices[2, 1] - vertices[0, 1]), 2));
-
-                for (int i = 0; i < lengthOfSides.Length; i++)
-                {
-                    Console.WriteLine($"Довжина {this.nameOfSides[i]}: {this.lengthOfSides[i]}");
-                }
-                Console.WriteLine("-----------------------------");
-            }
+            // To be implemented in child classes
         }
 
-        public double CalculateArea()
+        public virtual double CalculateArea()
         {
-            if (vertices == null || lengthOfSides == null)
-            {
-                Console.WriteLine("Координати не встановлені або довжини сторін не обчислені");
-                return 0;
-            }
-            else
-            {
-                double area, perimeter = 0;
-                for (int i = 0; i < CountOfVerticesTrian; i++)
-                {
-                    perimeter += lengthOfSides[i];
-                }
-                area = Math.Sqrt((perimeter / 2) * (perimeter / 2 - lengthOfSides[0]) * (perimeter / 2 - lengthOfSides[1]) * (perimeter / 2 - lengthOfSides[2]));
-                return area;
-            }
+            // To be implemented in child classes
+            return 0;
         }
     }
-}
 
-using System;
-
-namespace Inheritance
-{
-    class Quadrangle : Triangle
+    class Triangle : Shape
     {
-        private static int CountOfVert = 4;
-        public static int CountOfVerticesQuad
+        public Triangle()
         {
-            get { return CountOfVert; }
+            VertexCount = 3;
         }
 
-        private double[,] vertices;
-        private double[] lengthOfSides;
-        private string[] nameOfSides = { "AB", "BC", "CD", "AD" };
-
-        public new void SetVertices(double[,] vertices)
+        public override void CalculateSides()
         {
-            this.vertices = new double[CountOfVert, 2];
-            for (int i = 0; i < CountOfVert; i++)
-            {
-                this.vertices[i, 0] = vertices[i, 0];
-                this.vertices[i, 1] = vertices[i, 1];
-            }
+            // Implementation for calculating sides of a triangle
         }
 
-        public new void ShowVertices()
+        public override double CalculateArea()
         {
-            if (vertices == null)
-            {
-                Console.WriteLine("Координати не встановлені!");
-            }
-            else
-            {
-                Console.WriteLine("-----------------------------");
-                for (int i = 0; i < CountOfVert; i++)
-                {
-                    Console.WriteLine($"Вершина {i + 1}: x = {vertices[i, 0]}, y = {vertices[i, 1]}");
-                }
-                Console.WriteLine("-----------------------------");
-            }
+            // Implementation for calculating area of a triangle
+            return 0;
+        }
+    }
+
+    class Quadrangle : Shape
+    {
+        public Quadrangle()
+        {
+            VertexCount = 4;
         }
 
-        public new void CalculateSides()
+        public override void CalculateSides()
         {
-            Console.WriteLine("-----------------------------");
-            if (vertices == null)
-            {
-                Console.WriteLine("Координати не встановлені!");
-                Console.WriteLine("-----------------------------");
-            }
-            else
-            {
-                lengthOfSides = new double[CountOfVerticesQuad];
-                this.lengthOfSides[0] = Math.Sqrt(Math.Pow((vertices[1, 0] - vertices[0, 0]), 2) + Math.Pow((vertices[1, 1] - vertices[0, 1]), 2));
-                this.lengthOfSides[1] = Math.Sqrt(Math.Pow((vertices[2, 0] - vertices[1, 0]), 2) + Math.Pow((vertices[2, 1] - vertices[1, 1]), 2));
-                this.lengthOfSides[2] = Math.Sqrt(Math.Pow((vertices[3, 0] - vertices[2, 0]), 2) + Math.Pow((vertices[3, 1] - vertices[2, 1]), 2));
-                this.lengthOfSides[3] = Math.Sqrt(Math.Pow((vertices[3, 0] - vertices[0, 0]), 2) + Math.Pow((vertices[3, 1] - vertices[0, 1]), 2));
-
-                for (int i = 0; i < lengthOfSides.Length; i++)
-                {
-                    Console.WriteLine($"Довжина {this.nameOfSides[i]}: {this.lengthOfSides[i]}");
-                }
-                Console.WriteLine("-----------------------------");
-            }
+            // Implementation for calculating sides of a quadrangle
         }
 
-        public new double CalculateArea()
+        public override double CalculateArea()
         {
-            if (vertices == null || lengthOfSides == null)
-            {
-                Console.WriteLine("Координати не встановлені або довжини сторін не обчислені");
-                return 0;
-            }
-            else
-            {
-                double area = 0;
-                area = lengthOfSides[0] * lengthOfSides[1];
-                return area;
-            }
+            // Implementation for calculating area of a quadrangle
+            return 0;
         }
     }
 }
