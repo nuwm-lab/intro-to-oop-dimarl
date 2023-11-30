ccdc500b33ed714c8a5d6b0b46fac866f910e2cd
dimarl <115631397+dimarl@users.noreply.github.com>
2023-11-30 21:22:04 +0300

    Update lab6.cs

--- lab_6/lab6.cs
+++ lab_6/lab6.cs
@@ -4,18 +4,18 @@ namespace InterfacesAndAbstractClasses
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Quadrangle quad = new Quadrangle();
-            int[,] verticesQuad = new int[quad.NumberOfSides, 2];
-            string[] nameOfVerticesQuad = { "A", "B", "C", "D" };
+            var verticesQuad = new double[quad.NumberOfSides, 2];
+            var nameOfVerticesQuad = new string[] { "A", "B", "C", "D" };
 
             for (int i = 0; i < quad.NumberOfSides; i++)
             {
                 Console.Write($"Vertex {nameOfVerticesQuad[i]}: x = ");
-                verticesQuad[i, 0] = int.Parse(Console.ReadLine());
+                verticesQuad[i, 0] = double.Parse(Console.ReadLine());
                 Console.Write($"Vertex {nameOfVerticesQuad[i]}: y = ");
-                verticesQuad[i, 1] = int.Parse(Console.ReadLine());
+                verticesQuad[i, 1] = double.Parse(Console.ReadLine());
             }
 
             quad.SetVertices(verticesQuad);
@@ -24,15 +24,15 @@ namespace InterfacesAndAbstractClasses
             quad.ShowData();
 
             Triangle trian = new Triangle();
-            int[,] verticesTriangle = new int[trian.NumberOfSides, 2];
-            string[] nameOfVerticesTriangle = { "A", "B", "C" };
+            var verticesTriangle = new double[trian.NumberOfSides, 2];
+            var nameOfVerticesTriangle = new string[] { "A", "B", "C" };
 
             for (int i = 0; i < trian.NumberOfSides; i++)
             {
                 Console.Write($"Vertex {nameOfVerticesTriangle[i]}: x = ");
-                verticesTriangle[i, 0] = int.Parse(Console.ReadLine());
+                verticesTriangle[i, 0] = double.Parse(Console.ReadLine());
                 Console.Write($"Vertex {nameOfVerticesTriangle[i]}: y = ");
-                verticesTriangle[i, 1] = int.Parse(Console.ReadLine());
+                verticesTriangle[i, 1] = double.Parse(Console.ReadLine());
             }
 
             trian.SetVertices(verticesTriangle);
@@ -41,28 +41,17 @@ namespace InterfacesAndAbstractClasses
             trian.ShowData();
         }
     }
-}
 
-namespace InterfacesAndAbstractClasses
-{
     abstract class Figure
     {
-        public double Area { get; set; }
+        public double Area { get; protected set; }
         public abstract int NumberOfSides { get; }
         public abstract double CalculateArea();
         public abstract void CalculateSides();
-
-        /// <summary>
-        /// Sets the coordinates of the figure's vertices.
-        /// </summary>
-        /// <param name="vertices">Array of vertex coordinates</param>
-        public abstract void SetVertices(int[,] vertices);
+        public abstract void SetVertices(double[,] vertices);
         public abstract void ShowData();
     }
-}
 
-namespace InterfacesAndAbstractClasses
-{
     class Triangle : Figure
     {
         private double[,] vertices;
@@ -71,7 +60,7 @@ namespace InterfacesAndAbstractClasses
         private string[] namesOfVertices = { "A", "B", "C" };
         public override int NumberOfSides { get { return 3; } }
 
-        public override void SetVertices(int[,] vertices)
+        public override void SetVertices(double[,] vertices)
         {
             this.vertices = new double[NumberOfSides, 2];
             for (int i = 0; i < NumberOfSides; i++)
@@ -109,10 +98,15 @@ namespace InterfacesAndAbstractClasses
             else
             {
                 lengthsOfSides = new double[NumberOfSides];
-                lengthsOfSides[0] = Math.Sqrt(Math.Pow((vertices[1, 0] - vertices[0, 0]), 2) + Math.Pow((vertices[1, 1] - vertices[0, 1]), 2));
-                lengthsOfSides[1] = Math.Sqrt(Math.Pow((vertices[2, 0] - vertices[1, 0]), 2) + Math.Pow((vertices[2, 1] - vertices[1, 1]), 2));
-                lengthsOfSides[2] = Math.Sqrt(Math.Pow((vertices[2, 0] - vertices[0, 0]), 2) + Math.Pow((vertices[2, 1] - vertices[0, 1]), 2));
+                lengthsOfSides[0] = CalculateLength(vertices[0, 0], vertices[0, 1], vertices[1, 0], vertices[1, 1]);
+                lengthsOfSides[1] = CalculateLength(vertices[1, 0], vertices[1, 1], vertices[2, 0], vertices[2, 1]);
+                lengthsOfSides[2] = CalculateLength(vertices[2, 0], vertices[2, 1], vertices[0, 0], vertices[0, 1]);
             }
+        }
+
+        private double CalculateLength(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
         }
 
         public override void ShowData()
@@ -143,21 +137,18 @@ namespace InterfacesAndAbstractClasses
             }
         }
     }
-}
 
-namespace InterfacesAndAbstractClasses
-{
     class Quadrangle : Figure
     {
-        private int[,] vertices;
+        private double[,] vertices;
         private double[] lengthsOfSides;
         private string[] namesOfSides = { "AB", "BC", "CD", "AD" };
         private string[] namesOfVertices = { "A", "B", "C", "D" };
         public override int NumberOfSides { get { return 4; } }
 
-        public override void SetVertices(int[,] vertices)
+        public override void SetVertices(double[,] vertices)
         {
-            this.vertices = new int[NumberOfSides, 2];
+            this.vertices = new double[NumberOfSides, 2];
             for (int i = 0; i < NumberOfSides; i++)
             {
                 this.vertices[i, 0] = vertices[i, 0];
@@ -198,6 +189,7 @@ namespace InterfacesAndAbstractClasses
             if (lengthsOfSides == null || vertices == null)
             {
                 Console.WriteLine("Operation isn't available!");
+                return 0;
             }
 
             Area = lengthsOfSides[0] * lengthsOfSides[1];
@@ -214,11 +206,16 @@ namespace InterfacesAndAbstractClasses
             {
                 lengthsOfSides = new double[NumberOfSides];
 
-                lengthsOfSides[0] = Math.Sqrt(Math.Pow((vertices[1, 0] - vertices[0, 0]), 2) + Math.Pow((vertices[1, 1] - vertices[0, 1]), 2));
-                lengthsOfSides[1] = Math.Sqrt(Math.Pow((vertices[2, 0] - vertices[1, 0]), 2) + Math.Pow((vertices[2, 1] - vertices[1, 1]), 2));
-                lengthsOfSides[2] = Math.Sqrt(Math.Pow((vertices[3, 0] - vertices[2, 0]), 2) + Math.Pow((vertices[3, 1] - vertices[2, 1]), 2));
-                lengthsOfSides[3] = Math.Sqrt(Math.Pow((vertices[3, 0] - vertices[0, 0]), 2) + Math.Pow((vertices[3, 1] - vertices[0, 1]), 2));
+                lengthsOfSides[0] = CalculateLength(vertices[0, 0], vertices[0, 1], vertices[1, 0], vertices[1, 1]);
+                lengthsOfSides[1] = CalculateLength(vertices[1, 0], vertices[1, 1], vertices[2, 0], vertices[2, 1]);
+                lengthsOfSides[2] = CalculateLength(vertices[2, 0], vertices[2, 1], vertices[3, 0], vertices[3, 1]);
+                lengthsOfSides[3] = CalculateLength(vertices[3, 0], vertices[3, 1], vertices[0, 0], vertices[0, 1]);
             }
+        }
+
+        private double CalculateLength(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
         }
     }
 }
