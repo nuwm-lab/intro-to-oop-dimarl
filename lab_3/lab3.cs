8963774413c637096c87d86955c1f0b3e786ce80
dimarl <115631397+dimarl@users.noreply.github.com>
2023-11-30 20:41:56 +0300

    Update lab3.cs

--- lab_3/lab3.cs
+++ lab_3/lab3.cs
@@ -2,66 +2,79 @@ using System;
 
 namespace ArrayMatrix
 {
+    /// <summary>
+    /// Represents a matrix and provides operations on it.
+    /// </summary>
     class Matrix
     {
         private static Random rand = new Random();
-        private int rows;
-        private int columns;
-        private int[,] matrix;
+        private int numberOfRows;
+        private int numberOfColumns;
+        private int[,] matrixArray;
 
+        /// <summary>
+        /// Initializes a new instance of the Matrix class with the specified number of rows and columns.
+        /// </summary>
+        /// <param name="rows">The number of rows in the matrix.</param>
+        /// <param name="columns">The number of columns in the matrix.</param>
         public Matrix(int rows, int columns)
         {
-            this.rows = rows;
-            this.columns = columns;
-            matrix = new int[rows, columns];
+            numberOfRows = rows;
+            numberOfColumns = columns;
+            matrixArray = new int[rows, columns];
         }
 
-        ~Matrix()
-        {
-            Console.WriteLine("Memory cleaning...");
-        }
-
+        /// <summary>
+        /// Fills the matrix with random values.
+        /// </summary>
         public void FillMatrix()
         {
-            if (matrix == null)
+            if (matrixArray == null)
             {
-                matrix = new int[rows, columns];
+                matrixArray = new int[numberOfRows, numberOfColumns];
             }
 
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < numberOfRows; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (int j = 0; j < numberOfColumns; j++)
                 {
-                    matrix[i, j] = rand.Next(-20, 21);
+                    matrixArray[i, j] = rand.Next(-20, 21);
                 }
             }
         }
 
+        /// <summary>
+        /// Calculates the sum of the elements on the main diagonal of the matrix.
+        /// </summary>
+        /// <returns>The sum of the main diagonal elements.</returns>
         public int CalculateDiagonalSum()
         {
             int sum = 0;
 
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < numberOfRows; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (int j = 0; j < numberOfColumns; j++)
                 {
                     if (i == j)
                     {
-                        sum += matrix[i, j];
+                        sum += matrixArray[i, j];
                     }
                 }
             }
             return sum;
         }
 
+        /// <summary>
+        /// Displays the matrix elements.
+        /// </summary>
         public void ShowMatrix()
         {
             Console.WriteLine("-------------------------------------");
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < numberOfRows; i++)
             {
-                for (int j = 0; j < columns; j++)
+                for (int j = 0; j < numberOfColumns; j++)
                 {
-                    Console.Write($"{matrix[i, j],-10}");
+                    Console.Write($"{matrixArray[i, j],-10}");
                 }
                 Console.WriteLine();
             }
@@ -74,6 +87,9 @@ using System;
 
 namespace ArrayMatrix
 {
+    /// <summary>
+    /// Represents operations on matrices.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -84,11 +100,11 @@ namespace ArrayMatrix
             Console.Write("Enter the matrix size: ");
             int matrixSize = Convert.ToInt32(Console.ReadLine());
 
-            Matrix[] arrayOfMatrices = new Matrix[numOfMatrices]; 
+            Matrix[] arrayOfMatrices = new Matrix[numOfMatrices];
 
             for (int i = 0; i < arrayOfMatrices.Length; i++)
             {
-                arrayOfMatrices[i] = new Matrix(matrixSize, matrixSize); 
+                arrayOfMatrices[i] = new Matrix(matrixSize, matrixSize);
             }
 
             foreach (var matrix in arrayOfMatrices)
